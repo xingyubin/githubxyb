678306d55b6976f9a3bd337c092b79a0ef856c6f
xyb <740863116@qq.com>
2022-03-01 17:15:17 +0800

    a giteexyb

---
+++ src/main/java/com/atguigu/test/giteexybtest.java
@@ -0,0 +1,11 @@
+package com.atguigu.test;
+
+/**
+ * @author 邢钰彬
+ * @create 2022-03-01 17:14
+ */
+public class giteexybtest {
+    public static void main(String[] args) {
+        System.out.println("xxxxxx");
+    }
+}
